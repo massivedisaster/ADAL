6d544888c8eccf1e3a68cf2a9dd98dc80d7debee
Jorge Costa <j.costa90@gmail.com>
2016-12-15 12:04:23 +0000

    Only allow one account

--- adal/src/main/java/com/massivedisaster/adal/account/AccountAuthenticator.java
+++ adal/src/main/java/com/massivedisaster/adal/account/AccountAuthenticator.java
@@ -3,20 +3,47 @@ package com.massivedisaster.adal.account;
 import android.accounts.AbstractAccountAuthenticator;
 import android.accounts.Account;
 import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
 import android.accounts.NetworkErrorException;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import com.massivedisaster.adal.R;
 
 class AccountAuthenticator extends AbstractAccountAuthenticator {
 
+    private static final int ERROR_CODE_ONE_ACCOUNT_ALLOWED = 1001;
+
+    private Context mContext;
+
     AccountAuthenticator(Context context) {
         super(context);
+
+        mContext = context;
     }
 
     @Override
     public Bundle addAccount(AccountAuthenticatorResponse response, String accountType, String authTokenType, String[] requiredFeatures, Bundle options)
             throws NetworkErrorException {
-        // TODO Auto-generated method stub
+
+        if (AccountHelper.hasAccount(mContext)) {
+            final Bundle result = new Bundle();
+            result.putInt(AccountManager.KEY_ERROR_CODE, ERROR_CODE_ONE_ACCOUNT_ALLOWED);
+            result.putString(AccountManager.KEY_ERROR_MESSAGE, mContext.getString(R.string.one_account_allowed));
+
+            final Handler handler = new Handler();
+            handler.post(new Runnable() {
+
+                @Override
+                public void run() {
+                    Toast.makeText(mContext, R.string.one_account_allowed, Toast.LENGTH_SHORT).show();
+                }
+            });
+            return result;
+        }
+
         return null;
     }
 
