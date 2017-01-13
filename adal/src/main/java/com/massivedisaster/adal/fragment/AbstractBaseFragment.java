21f2bdd634365d0f4fdf32155bad3bc2b527cb6b
Jorge Costa <j.costa90@gmail.com>
2017-01-13 18:53:35 +0000

    Added method to protect click when fragment are in an animation

--- adal/src/main/java/com/massivedisaster/adal/fragment/AbstractBaseFragment.java
+++ adal/src/main/java/com/massivedisaster/adal/fragment/AbstractBaseFragment.java
@@ -1,6 +1,9 @@
 package com.massivedisaster.adal.fragment;
 
 import android.support.v4.app.Fragment;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.massivedisaster.adal.util.KeyboardUtils;
 
@@ -11,5 +14,38 @@ public abstract class AbstractBaseFragment extends Fragment {
         KeyboardUtils.hide(getActivity(), getView());
 
         super.onPause();
+    }
+
+    @Override
+    public Animation onCreateAnimation(int transit, boolean enter, int nextAnim) {
+        //Check if the superclass already created the animation
+        Animation anim = super.onCreateAnimation(transit, enter, nextAnim);
+
+        if (anim == null && nextAnim != 0) {
+            anim = AnimationUtils.loadAnimation(getActivity(), nextAnim);
+        }
+
+        if (anim != null) {
+            anim.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+        }
+
+        return anim;
     }
 }
