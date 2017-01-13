92d036fc3384189460ee032619242297f6836973
Jorge Costa <j.costa90@gmail.com>
2017-01-13 18:54:52 +0000

    Added centertitle funciton to center title in a toolbar

---
+++ adal/src/main/java/com/massivedisaster/adal/util/ToolbarUtil.java
@@ -0,0 +1,26 @@
+package com.massivedisaster.adal.util;
+
+import android.app.Activity;
+import android.view.View;
+
+/**
+ * Created by Jorge Costa on 13/01/17.
+ */
+
+public class ToolbarUtil {
+
+    public static void centerTitle(final Activity a, final View v) {
+        v.post(new Runnable() {
+            @Override
+            public void run() {
+                int marginRight = a.getWindow().getDecorView().getWidth() - v.getRight();
+
+                if (marginRight > v.getLeft()) {
+                    v.setPadding(marginRight - v.getLeft(), 0, 0, 0);
+                } else {
+                    v.setPadding(0, 0, v.getLeft() - marginRight, 0);
+                }
+            }
+        });
+    }
+}
