c1efc36e2d51e7737b6c6a2cebf5966429016e29
extmkv <j.costa90@gmail.com>
2017-02-15 12:48:42 +0000

    Removed colorAccent

--- adal/src/main/java/com/massivedisaster/adal/util/SnackBuilder.java
+++ adal/src/main/java/com/massivedisaster/adal/util/SnackBuilder.java
@@ -4,20 +4,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
 
-import com.massivedisaster.adal.R;
-
 public class SnackBuilder {
 
     public static void show(View v, int messageRes, int actionColor) {
         show(v, v.getContext().getString(messageRes), actionColor);
-    }
-
-    public static void show(View v, int messageRes) {
-        show(v, v.getContext().getString(messageRes), R.color.colorAccent);
-    }
-
-    public static void show(View view, String message) {
-        show(view, message, R.color.colorAccent);
     }
 
     public static void show(View view, String message, int actionColor) {
