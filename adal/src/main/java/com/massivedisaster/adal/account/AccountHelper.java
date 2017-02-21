40afd7998fe29f1e5eff3b8256c6d78701343767
Jorge Costa <j.costa90@gmail.com>
2017-02-21 17:45:22 +0000

    Added doc to account helper

--- adal/src/main/java/com/massivedisaster/adal/account/AccountHelper.java
+++ adal/src/main/java/com/massivedisaster/adal/account/AccountHelper.java
@@ -13,10 +13,23 @@ public class AccountHelper {
     private static AccountManager mManager;
     private static int deletedAccounts;
 
+    /**
+     * Initialize the AccountHelper
+     *
+     * @param context The application context
+     */
     public static void initialize(Context context) {
         mManager = AccountManager.get(context);
     }
 
+    /**
+     * Add a new account to the account manager
+     *
+     * @param context  The application context.
+     * @param name     The account name.
+     * @param password The account password.
+     * @param token    The account token.
+     */
     public static void addAccount(final Context context, final String name, final String password, final String token) {
         validateAccountManager();
         clearAccounts(context, new OnAccountListener() {
@@ -74,24 +87,48 @@ public class AccountHelper {
         }
     }
 
+    /**
+     * Verify if the application has account added.
+     *
+     * @param context The application context.
+     * @return True if the application has a account added.
+     */
     @SuppressWarnings("MissingPermission")
     public static boolean hasAccount(Context context) {
         validateAccountManager();
         return mManager.getAccountsByType(context.getPackageName()).length > 0;
     }
 
+    /**
+     * Verify if the application  have a account and retrieve the account
+     *
+     * @param context The application context.
+     * @return The current account.
+     */
     @SuppressWarnings("MissingPermission")
     public static Account getCurrentAccount(Context context) {
         validateAccountManager();
         return mManager.getAccountsByType(context.getPackageName())[0];
     }
 
+    /**
+     * Verify if the application have an account and retrieve the account password
+     *
+     * @param account The account.
+     * @return The user password.
+     */
     @SuppressWarnings("MissingPermission")
     public static String getAccountPassword(Account account) {
         validateAccountManager();
         return mManager.getPassword(account);
     }
 
+    /**
+     * Verify if the application have an account and retrieve the account token
+     *
+     * @param context The application context.
+     * @return The account token.
+     */
     @SuppressWarnings("MissingPermission")
     public static String getCurrentToken(Context context) {
         validateAccountManager();
