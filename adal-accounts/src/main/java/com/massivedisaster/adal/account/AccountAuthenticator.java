d324146c171984869eed8e3647b558afa6278a36
jzeferino <jorgevalentzeferino@gmail.com>
2017-04-28 11:48:29 +0100

    Removed auto generated commented code.

--- adal-accounts/src/main/java/com/massivedisaster/adal/account/AccountAuthenticator.java
+++ adal-accounts/src/main/java/com/massivedisaster/adal/account/AccountAuthenticator.java
@@ -48,37 +48,31 @@ class AccountAuthenticator extends AbstractAccountAuthenticator {
 
     @Override
     public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account, Bundle options) throws NetworkErrorException {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public String getAuthTokenLabel(String authTokenType) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account, String[] features) throws NetworkErrorException {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
-        // TODO Auto-generated method stub
         return null;
     }
 }
